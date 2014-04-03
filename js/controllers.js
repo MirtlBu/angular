5d6601436d79bcfefa3e7bef2fd2658f0441ae64
Mirtl <bubuzoglo@gmail.com>
2014-04-03 18:29:20 +0400

    few corrections and lib folder

--- js/controllers.js
+++ js/controllers.js
@@ -6,10 +6,7 @@ angular.module('vimeoApp.controllers', []).
         vimeoAppService.getVideo()
             .success(function(data){
                 $scope.videoList = data;
-                for(var i = 0; i < $scope.videoList.length; i++){
-                   var foo = $scope.videoList[i].description.replace(/\"/gi, '');
-                   $scope.videoList[i].description = foo;
-                }
                 $scope.predicate = "stats_number_of_plays";
             });
+
     });
